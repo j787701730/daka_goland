1268a6c298439ca481b902806d764c9fd331df39
zhengw <247276359@qq.com>
2021-02-19 17:15:38 +0800

    使用json来配置提示时间和内容.

--- index.go
+++ index.go
@@ -1,10 +1,69 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+	"sync"
 	"syscall"
 	"time"
 	"unsafe"
 )
+
+type Configs map[string]json.RawMessage
+
+var configPath string = "./config.json"
+
+type Desc struct {
+	Time    string `json:"time"`
+	Title   string `json:"title"`
+	Message string `json:"message"`
+}
+
+type MainConfig struct {
+	Data []Desc `json:"data"`
+}
+
+var conf *MainConfig
+var confs Configs
+
+var instanceOnce sync.Once
+
+//从配置文件中载入json字符串
+func LoadConfig(path string) (Configs, *MainConfig) {
+	buf, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Panicln("load config conf failed: ", err)
+	}
+	mainConfig := &MainConfig{}
+	err = json.Unmarshal(buf, mainConfig)
+	if err != nil {
+		log.Panicln("decode config file failed:", string(buf), err)
+	}
+	allConfigs := make(Configs, 0)
+	err = json.Unmarshal(buf, &allConfigs)
+	if err != nil {
+		log.Panicln("decode config file failed:", string(buf), err)
+	}
+
+	return allConfigs, mainConfig
+}
+
+func Init(path string) *MainConfig {
+	if conf != nil && path != configPath {
+		log.Printf("the config is already initialized, oldPath=%s, path=%s", configPath, path)
+	}
+	instanceOnce.Do(func() {
+		allConfigs, mainConfig := LoadConfig(path)
+		configPath = path
+		conf = mainConfig
+		confs = allConfigs
+	})
+
+	return conf
+}
 
 func IntPtr(n int) uintptr {
 	return uintptr(n)
@@ -27,6 +86,20 @@ func ShowMessage2(title, text string) {
 }
 
 func main() {
+	path := configPath
+	//fmt.Println("path: ", path)
+	Init(path)
+	value := confs["data"]
+	//fmt.Println(string(value))
+
+	var data []Desc
+	err := json.Unmarshal([]byte(value), &data)
+	//fmt.Println(strings.Split(data[0].Time, ":")[0])
+	//fmt.Println(len(data))
+	if err != nil {
+		log.Panicln("decode config file failed:", string("x"), err)
+	}
+
 	ticker := time.NewTicker(time.Second * 1)
 	ch := make(chan int)
 	day := time.Now().Day()
@@ -35,36 +108,23 @@ func main() {
 			select {
 			case <-ticker.C:
 				now := time.Now()
-				hour := now.Hour()
-				minute := now.Minute()
+				hour := strconv.Itoa(now.Hour())
+				minute := strconv.Itoa(now.Minute())
 				if day != now.Day() {
 					day = now.Day()
 					closeFlag = false
 				}
-				//fmt.Println(time.Now().Hour())
-				if hour == 9 {
-					if minute == 55 && !closeFlag {
-						ShowMessage2("打卡提示", "早上上班打卡")
-					} else if minute != 55 && closeFlag {
-						closeFlag = false
-					}
-				} else if hour == 11 {
-					if minute == 49 && !closeFlag {
-						ShowMessage2("午餐提示", "早上下班打卡, 身体革命本钱, 人是铁饭是钢! ")
-					} else if minute != 49 && closeFlag {
-						closeFlag = false
-					}
-				} else if hour == 13 {
-					if minute == 28 && !closeFlag {
-						ShowMessage2("打卡提示", "下午上班打卡")
-					} else if minute != 28 && closeFlag {
-						closeFlag = false
-					}
-				} else if hour == 17 {
-					if minute == 39 && !closeFlag {
-						ShowMessage2("打卡提示", "下午下班打卡")
-					} else if minute != 39 && closeFlag {
-						closeFlag = false
+
+				for j := 0; j < len(data); j++ {
+					dataTime := strings.Split(data[j].Time, ":")
+					//	strings.Split(data[0].Time, ":")[0]
+					if hour == dataTime[0] {
+						//fmt.Println(hour)
+						if minute == dataTime[1] && !closeFlag {
+							ShowMessage2(data[j].Title, data[j].Message)
+						} else if minute != dataTime[1] && closeFlag {
+							closeFlag = false
+						}
 					}
 				}
 			}
